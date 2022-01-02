21a72e4c21c0d8cf5044cfa76ba8bf4e2c897014
Damien Dumontet <damiendumontet@MacBook-Air-de-Damien.home>
2022-01-02 02:05:05 +0100

    start landing page and finish config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
-import { defineNuxtConfig } from "nuxt3";
+import { defineNuxtConfig } from 'nuxt3';
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  css: ["@/assets/css/styles.css"],
+  css: ['@/assets/css/styles.css'],
+  meta: {
+    link: [
+      {
+        rel: 'stylesheet',
+        href: 'https://cdn.iconmonstr.com/1.3.0/css/iconmonstr-iconic-font.min.css',
+      },
+    ],
+  },
   build: {
     postcss: {
-      postcssOptions: require("./postcss.config.js"),
+      postcssOptions: require('./postcss.config.js'),
     },
   },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1 class="text-3xl text-red-500">Hello World!</h1>
+  <div class="h-screen w-screen p-8">
+    <div class="h-full w-full">
+      <div class="h-1/2 flex items-center justify-center flex-col relative">
+        <h1 class="text-6xl font-black">Where do you</h1>
+        <h1 class="text-sky-400 text-6xl font-black">want to go ?</h1>
+        <div
+          class="h-20 w-1/2 absolute bg-white -bottom-10 z-10 shadow rounded-full flex items-center pl-2 pr-2"
+        >
+          <div
+            class="h-16 w-16 bg-sky-400 rounded-full ml-auto flex items-center justify-center hover:bg-sky-500 cursor-pointer transition duration-300"
+          >
+            <i class="im im-magnifier text-white"></i>
+          </div>
+        </div>
+      </div>
+      <div class="h-1/2 bg-sky-400 flex items-center justify-center relative">
+        <img src="/assets/images/chair.png" class="h-64 invert" />
+        <img src="/assets/images/sand.png" class="h-64 invert" />
+        <img src="/assets/images/bikini.png" class="h-64 invert" />
+        <img src="/assets/images/shoes.png" class="h-64 invert" />
+      </div>
+    </div>
   </div>
 </template>
